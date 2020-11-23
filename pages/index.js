1ff1978843587695a45d41f7e8d83c567c80e800
kb1995 <kristiyan.r.bogdanov@gmail.com>
2020-11-23 10:38:23 +0000

    Add title

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,15 @@ import SliceZone from "next-slicezone";
 import { useGetStaticProps } from "next-slicezone/hooks";
 
 import resolver from "../sm-resolver.js";
+import { NextSeo } from "next-seo";
 
 const Page = (props) => (
-  <div className="mb-8 grid grid-cols-1 gap-y-16 lg:gap-y-28">
-    <SliceZone {...props} resolver={resolver} />
-  </div>
+  <>
+    <NextSeo title="Kris' Slice Machine" />
+    <div className="mb-8 grid grid-cols-1 gap-y-16 lg:gap-y-28">
+      <SliceZone {...props} resolver={resolver} />
+    </div>
+  </>
 );
 
 // Fetch content from prismic
